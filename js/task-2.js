00ae73aa5b4e885f4b5f85ac4bca2a65c15978a3
Dariashevc <151487077+Dariashevc@users.noreply.github.com>
2024-07-20 19:41:02 -0600

    commit

--- js/task-2.js
+++ js/task-2.js
@@ -1,22 +1,22 @@
 class Storage {
-	#items;
 
-	constructor(items) {
-		this.#items = items;
-	}
-
-	getItems() {
-		return this.#items;
-	}
-
-	addItem(newItem) {
-		this.#items.push(newItem);
-	}
-
-	removeItem(itemToRemove) {
-		const index = this.#items.indexOf(itemToRemove);
-		this.#items.splice(index, 1);
-	}
+    #items;
+     
+    constructor(items) {
+        this.#items = items;
+    }
+     
+    getItems() {
+        return this.#items;
+    }
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter(item =>
+ item !== itemToRemove);
+        
+    }
 }
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
